0bc77bd9c66675b2ba7ff078c3ef300da5548731
Moritz Degen <moritz.degen@h2powerheat.de>
2026-05-08 12:46:10 +0200

    changed HW_POST_MAKE to ar.exe

--- C0_C/env/project.mk
+++ C0_C/env/project.mk
@@ -82,7 +82,7 @@ PC_PRE_MAKE     =
 #PC_POST_MAKE    =
 
 # create a lib for ECU (*.obj -> *.lib)
-HW_POST_MAKE = "$(TI_5XX_PATH)\bin\armar.exe" -r "src\app_libs\CEN_$(TARGET_BIG)_HW_AppFunc.lib" "_debug\cen_hw\$(TARGET_BIG)\src\app_libs\AppFunc.obj"
+HW_POST_MAKE = "$(MINGW_PATH)\bin\ar.exe" rcs "src\app_libs\CEN_$(TARGET_BIG)_HW_AppFunc.a" "_debug\cen_hw\$(TARGET_BIG)\src\app_libs\AppFunc.obj"
 # Post make command for the pc build
 # build a library for pc, libs with name "CEN_$(TARGET_BIG)_PC_*.a" will be automatically
 #used after clean for pc build
