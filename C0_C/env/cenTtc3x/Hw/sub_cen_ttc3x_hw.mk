462bf3e10a2482eab9dfdaf0c89dc18e6222d1bc
Moritz Degen <moritz.degen@h2powerheat.de>
2026-05-08 12:40:46 +0200

    stuff idk

--- C0_C/env/cenTtc3x/Hw/sub_cen_ttc3x_hw.mk
+++ C0_C/env/cenTtc3x/Hw/sub_cen_ttc3x_hw.mk
@@ -10,29 +10,34 @@ SUB_CC_INCLUDES = \
 -I"$(ROOT_PATH)/include/library/standard"  \
 -I"$(ROOT_PATH)/include/library/testsuite"  \
 -I"$(ROOT_PATH)/include/library/testsuite/cvtest"  \
+-I"$(ROOT_PATH)/include/protocol"  \
+-I"$(ROOT_PATH)/include/protocol/PtcMst"  \
 -I"$(ROOT_PATH)/src" \
+-I"$(ROOT_PATH)/src/app_libs"  \
 -I"$(ROOT_PATH)/src/_app"  \
 -I"$(ROOT_PATH)/src/_app/2App"  \
 -I"$(ROOT_PATH)/src/_app/AUTO_CEN_TTC3X" 
 
 
 SUB_LIB_EXT = \
-$(ROOT_PATH)/include/_lib/TTC32S/hw/core/CEN_TTC32S_HW_CoreBasStd.lib
+$(ROOT_PATH)/include/_lib/TTC32S/hw/core/CEN_TTC32S_HW_CoreBasStd.lib \
+$(ROOT_PATH)/include/_lib/TTC32S/hw/protocol/CEN_TTC32S_HW_PtcMst.lib
 
 
 SUB_OBJECTS = \
+$(OUT_PATH)/src/app_libs/AppFunc.obj \
 $(OUT_PATH)/src/main_cen.obj \
 $(OUT_PATH)/src/_app/2App/2AppCode.obj \
 $(OUT_PATH)/src/_app/AppBspMem.obj \
 $(OUT_PATH)/src/_app/AppCode.obj \
-$(OUT_PATH)/src/_app/AUTO_CEN_TTC3X/App_Db.obj \
 $(OUT_PATH)/src/_app/AUTO_CEN_TTC3X/App_LibTab.obj \
 $(OUT_PATH)/src/_app/AUTO_CEN_TTC3X/App_Pin.obj \
+$(OUT_PATH)/src/_app/AUTO_CEN_TTC3X/Cfg_CBus.obj \
 $(OUT_PATH)/src/_app/AUTO_CEN_TTC3X/Cfg_Ecu.obj \
 $(OUT_PATH)/src/_app/AUTO_CEN_TTC3X/Cfg_LibRegistry.obj \
+$(OUT_PATH)/src/_app/AUTO_CEN_TTC3X/Cfg_PtcMst.obj \
 $(OUT_PATH)/src/_app/AUTO_CEN_TTC3X/Cfg_Sil.obj \
-$(OUT_PATH)/src/_app/AUTO_CEN_TTC3X/Cfg_Ver.obj \
-$(OUT_PATH)/src/_app/AUTO_CEN_TTC3X/Cfg_VerExt.obj
+$(OUT_PATH)/src/_app/AUTO_CEN_TTC3X/Cfg_Ver.obj
 
 
 SUB_LINT = \
@@ -45,19 +50,23 @@ SUB_LINT = \
 -I$(LINT_PATH)\include\library\standard \
 -I$(LINT_PATH)\include\library\testsuite \
 -I$(LINT_PATH)\include\library\testsuite\cvtest \
+-I$(LINT_PATH)\include\protocol \
+-I$(LINT_PATH)\include\protocol\PtcMst \
 -I$(LINT_PATH)\src \
+-I$(LINT_PATH)\src\app_libs \
 -I$(LINT_PATH)\src\_app \
 -I$(LINT_PATH)\src\_app\2App \
 -I$(LINT_PATH)\src\_app\AUTO_CEN_TTC3X \
+	$(LINT_PATH)\src\app_libs\AppFunc.c \
 	$(LINT_PATH)\src\main_cen.c \
 	$(LINT_PATH)\src\_app\2App\2AppCode.c \
 	$(LINT_PATH)\src\_app\AppBspMem.c \
 	$(LINT_PATH)\src\_app\AppCode.c \
-	$(LINT_PATH)\src\_app\AUTO_CEN_TTC3X\App_Db.c \
 	$(LINT_PATH)\src\_app\AUTO_CEN_TTC3X\App_LibTab.c \
 	$(LINT_PATH)\src\_app\AUTO_CEN_TTC3X\App_Pin.c \
+	$(LINT_PATH)\src\_app\AUTO_CEN_TTC3X\Cfg_CBus.c \
 	$(LINT_PATH)\src\_app\AUTO_CEN_TTC3X\Cfg_Ecu.c \
 	$(LINT_PATH)\src\_app\AUTO_CEN_TTC3X\Cfg_LibRegistry.c \
+	$(LINT_PATH)\src\_app\AUTO_CEN_TTC3X\Cfg_PtcMst.c \
 	$(LINT_PATH)\src\_app\AUTO_CEN_TTC3X\Cfg_Sil.c \
-	$(LINT_PATH)\src\_app\AUTO_CEN_TTC3X\Cfg_Ver.c \
-	$(LINT_PATH)\src\_app\AUTO_CEN_TTC3X\Cfg_VerExt.c
+	$(LINT_PATH)\src\_app\AUTO_CEN_TTC3X\Cfg_Ver.c
--- C0_C/src/autoconfig.h
+++ C0_C/src/autoconfig.h
@@ -7,15 +7,15 @@
 
 #ifndef __AUTOCONFIG__
 	#define __AUTOCONFIG__
-		
-	#define COMPILER_SWITCH_COM_HIGHTEC      // Used Compiler
-	#define COMPILER_SWITCH_ENV_CEN          // Used OS 
-	#define COMPILER_SWITCH_DATA_INTEL       // Used Data Formats
-	#define COMPILER_SWITCH_TAR_HW           // Used Target
-	#define COMPILER_SWITCH_OEM_TTC          // Used OEM 
-    #define COMPILER_SWITCH_OS_PXROS         // Used RTOS
-	#define COMPILER_SWITCH_PLATFORM_TTC2XXX // Used Controller platform
-	#define COMPILER_SWITCH_FAM_TTC20XX      // Used Ecu Family
-	#define COMPILER_SWITCH_ECU_TTC2038      // Used Ecu
-  	
+
+	#define COMPILER_SWITCH_COM_MINGW        // Used Compiler
+	#define COMPILER_SWITCH_ENV_CEN          // Used OS
+	#define COMPILER_SWITCH_DATA_INTEL       // Used Data Formats   
+	#define COMPILER_SWITCH_TAR_PC           // Used Target
+	#define COMPILER_SWITCH_OEM_TTC          // Used OEM
+	#define COMPILER_SWITCH_FAM_TTC3X        // Used Ecu Family
+	#define COMPILER_SWITCH_FAM_TTC3X_SAFETY // Used Ecu Family Safety
+	#define COMPILER_SWITCH_SUBFAM_TTC32     // Used Ecu Sub Family 
+	#define COMPILER_SWITCH_ECU_TTC32S       // Used Ecu
+
 #endif
